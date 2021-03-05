65a2053149ac30517ec79a49c7abdf426908833b
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-03-05 21:25:26 +0200

    style added

--- public/main.js
+++ public/main.js
@@ -12,6 +12,8 @@ const centerDiv = document.getElementById("center");
 const statisticsSection = document.getElementById("statisticsSection");
 const searchInput = document.getElementById("searchUrl");
 const searchButton = document.getElementById("search");
+const statistics = document.getElementById("statistics");
+const controlSection = document.getElementById("controlSection");
 
 button.addEventListener("click", () => {
   if (input.value != "") {
@@ -27,32 +29,30 @@ function shortUrlLink(shortUrl) {
   linkSpan.innerHtml = "";
   linkSpan.innerText = "";
   const link = document.createElement("a");
-  link.innerText = server + "shortURL/new/" + shortUrl;
-  // const linkMessage =
-  //   "This: " + input.value + " and his short version id :  \n";
-  // linkSpan.innerText = linkMessage;
+  link.innerText = "\n" + server + "shortURL/new/" + shortUrl;
   link.setAttribute("href", link.innerText);
   linkSpan.append(link);
-  centerDiv.append(linkSpan);
+  controlSection.append(linkSpan);
   input.value = "";
 }
 
-function newElem(type, className) {
+function newElem(type, idName) {
   let elem = document.createElement(type);
-  if (className) elem.className = className;
+  if (idName) elem.id = idName;
   return elem;
 }
 
 function showStatistics(creationDate, redirectCount, originalUrl, shorturl) {
-  const creationDateDiv = newElem("div", "creationDateStat");
+  statistics.innerHTML = "";
+  const creationDateDiv = newElem("span", "creationDateStat");
   const redirectCountDiv = newElem("div", "redirectCountStat");
   const originalUrlDiv = newElem("div", "originalUrlStat");
   const shortUrlDiv = newElem("div", "shortlUrStat");
-  creationDateDiv.innerText = creationDate;
-  redirectCountDiv.innerText = redirectCount;
-  originalUrlDiv.innerText = originalUrl;
-  shortUrlDiv.innerText = shorturl;
-  statisticsSection.append(
+  creationDateDiv.innerText = " Creation Date:\n" + creationDate;
+  redirectCountDiv.innerText = "Redirect Count :\n" + redirectCount;
+  originalUrlDiv.innerText = "Original URL : \n" + originalUrl;
+  shortUrlDiv.innerText = "Shorted URL : \n" + shorturl;
+  statistics.append(
     creationDateDiv,
     redirectCountDiv,
     shortUrlDiv,
